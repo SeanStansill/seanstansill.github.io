ba256f154e3d69b8aa3b754980a685d2d70e308b
SeanStansill <ll14s26s@leeds.ac.uk>
2023-01-22 14:31:01 +0000

    Add error pages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/stores';
+
+	const emojis = {
+		// TODO add the rest!
+		420: '🫠',
+		500: '💥'
+	};
+</script>
+
+<div class='innerEdgeCentred'>
+    <h1>{$page.status} {$page.error.message}</h1>
+</div>
+
+<div class='innerEdgeCentred'>
+    <span style="font-size: 10em">
+        {emojis[$page.status] ?? emojis[500]}
+    </span>
+</div>

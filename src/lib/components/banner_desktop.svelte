f61e19b0af8b6b4aac4400802d2dbf4c0f89f467
SeanStansill <ll14s26s@leeds.ac.uk>
2023-01-25 15:12:07 +0000

    Add mobile banner support and document banner

--- src/lib/components/banner_desktop.svelte
+++ src/lib/components/banner_desktop.svelte
@@ -1,31 +1,27 @@
 <script>
     export let image_url;
 
+    // Initialize variable which we will assign to
+    // the y location
     let scroll;
 </script>
 
+<!-- Bind the variable scroll to the y location -->
 <svelte:window bind:scrollY={scroll}/>
 
+<!-- Add the banner-->
 <div class='outerEdgeCentred' style='background-color:white; z-index=-1; position:relative;'>
+    <!-- style: is a new Svelte feature. Sugar coating for dynamic reactive CSS styling -->
     <img src='{image_url}' alt='Page banner' class='banner_desktop' style:transform={`translate3d(0, ${scroll * 0.3}px, 0)`}/>
 </div>
 
 
 <style>
-    .background_desktop {
-        width: 100%;
-        height:400px;
-        background-attachment: fixed; 
-        background-position: center 0%;
-        background-repeat: no-repeat; 
-        background-size: contain;
-        overflow: hidden;
-    }
     .banner_desktop {
         width: 100%;
         height:400px;
         object-fit: cover;
-        object-position: center 0%;;
+        object-position: center 10%;;
         overflow: hidden;
     }
 </style>
--- src/lib/components/banner_mobile.svelte
+++ src/lib/components/banner_mobile.svelte
@@ -1,16 +1,25 @@
 <script>
     export let image_url;
+
+    let scroll;
 </script>
 
-<div class='background_mobile' style='background-image: url({image_url})'></div>
+<svelte:window bind:scrollY={scroll}/>
+
+<!-- Add the banner-->
+
+<div style='background-color:white; z-index=-1; position:relative;'>
+    <!-- style: is a new Svelte feature. Sugar coating for dynamic reactive CSS styling -->
+    <img src='{image_url}' alt='Page banner' class='banner_mobile' style:transform={`translate3d(0, ${scroll * 0.5}px, 0)`}/>
+</div>
 
 <style>
-    .background_mobile {
-        width:100%;
-        height:400px;
-        background-attachment: fixed; 
-        background-position: center 0%; 
-        background-repeat: no-repeat; 
-        background-size: contain;
+    .banner_mobile {
+        width: 100%;
+        max-height:200px;
+        position: relative;
+        object-fit: cover;
+        object-position: center 15%;;
+        overflow: hidden;
     }
 </style>

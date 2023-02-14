8721c7234ed1733d2030a5888f2479a6e27f1a5f
SeanStansill <ll14s26s@leeds.ac.uk>
2023-02-14 14:30:51 +0000

    Revert "Update isMobile.js max viewport"
    This reverts commit 4a0e30cb5d1ad1b42f0a2ab48ef611e6e58087c7.

--- src/lib/components/isMobile.js
+++ src/lib/components/isMobile.js
@@ -9,7 +9,7 @@ import { readable } from 'svelte/store';
 export const isMobile = readable(true, (set) => {
 	if (typeof window === 'undefined') return () => {};
 
-	const query = window.matchMedia('(max-width: 800px)');
+	const query = window.matchMedia('(max-width: 1000px)');
 	set(query.matches);
 	const update = (match) => {
 		set(match.matches);
